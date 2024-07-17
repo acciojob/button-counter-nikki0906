d18b32326f2e85a3bdd2e3bad6ec367032264a71
Nikki Tiwari <nikkitiwari915@gmail.com>
2024-07-17 12:11:44 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [count, setCount] = React.useState(0);
+  let[count, setCount] = useState(0);
+
+  function click(){
+    setCount(count + 1)
+  }
   return (
     <div>
-        <p style = {{fontWeight : "bold"}}> Button Clicked {count} times</p>
-        <button onClick = {() => setCount(count + 1)}>Click Me</button>
+        <p>Button Clicked {count} times</p>
+        <button onClick = {click}>Click Me</button>
     </div>
   );
 };
